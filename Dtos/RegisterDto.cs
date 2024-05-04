c8adcbce9fc3ddb10174f6c95ccf4429c00df4f7
sadeepa-dilshan <S19002535@ousl.lk>
2024-05-04 20:03:10 +0530

    Add project files.

---
+++ Dtos/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        public string Password { get; set; }
+
+        public List<string>? Roles { get; set; }
+    }
+}
